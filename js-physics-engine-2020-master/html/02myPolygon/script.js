a67b032359382c23559728367e94bd850e425ad5
dano553 <55272289+dano553@users.noreply.github.com>
2020-04-03 14:07:34 +0200

    Add files via upload

--- js-physics-engine-2020-master/html/02myPolygon/script.js
+++ js-physics-engine-2020-master/html/02myPolygon/script.js
@@ -8,14 +8,16 @@ canvas.width = width;
 canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
-let points = [];
-
-for (let i=0; i<3;i++)
-{
-  let x = getRandomInt(0,width);
-  let y = getRandomInt(0,height);
-  let myPoint = new Point(x,y,5,"black",i);
-  points.push(myPoint)
+let points =[];
+for(let i = 0; i<10; i++){
+  let x = getRandomInt(0, width);
+  let y = getRandomInt(0, height);
+  let myPoint = new Point(x,y,5,"black", i);
+  points.push(myPoint);
 }
-let myPolygon = new Polygon(points);
-myPolygon.draw(context);
+
+
+let color = ["red"];
+
+let mypolygon = new Polygon(points, color[0]);
+mypolygon.draw();
